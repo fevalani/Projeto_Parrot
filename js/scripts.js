926a3f4dd20d0822b9d49ba4f97f6f9df3e4ffc8
Fernando Valani Cavalcante <valanife@gmail.com>
2021-04-07 12:12:03 -0300

    selecionar 2 cartas

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,8 @@ function jogar(){
             qtd = 0;
         }
     }*/
-    qtd = 14;
+    qtd = 6;
+    //document.getElementsByClassName('container').style.width = "100px";
     distribuirCartas();
 }
 
@@ -16,14 +17,22 @@ function distribuirCartas(){
     i = 0;
     cartas = [];
     while(i < qtd){
-        cartas[i] = `<li onclick="virarCarta()"><img src="./imagens/front.png" alt="papagaio"></li>`;
+        cartas[i] = `<li onclick="virarCarta(${i})"><img src="./imagens/front.png" alt="papagaio"></li>`;
         i++;
     }
     distribuir.innerHTML = cartas;
 }
 
 
-function virarCarta(){
-    const virar = document.querySelector("li:nth-child(4) img");
-    virar.setAttribute("src", "./imagens/bobrossparrot.gif");
+let virada = 0;
+function virarCarta(n){
+    const valor = n + 1;
+    if(virada < 2){
+        const virar = document.querySelector("li:nth-child("+valor+") img");
+        virar.setAttribute("src", "./imagens/bobrossparrot.gif");
+        virada = virada + 1;
+        console.log(virada);
+    } else{
+        alert("duas cartas já selecionadas");
+    }
 }
