894cde40b69deb6dd6bc52dcac53607c2b082eeb
Fernando Valani Cavalcante <valanife@gmail.com>
2021-04-07 00:12:48 -0300

    Distribuir cartas feito

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,29 @@
+let qtd = 0;
 jogar();
 function jogar(){
-    let qtd = 0;
-    while(qtd > 14 | qtd < 4){
+    /*while(qtd > 14 | qtd < 4){
         qtd = parseInt(prompt("Digite com quatas cartas quer jogar: "));
         if(qtd%2 !== 0){
-            alert("Impossível jogar com número ímpar!!")
             qtd = 0;
         }
-    }
+    }*/
+    qtd = 14;
+    distribuirCartas();
+}
 
-    alert(qtd);
+function distribuirCartas(){
+    const distribuir = document.querySelector("ul");
+    i = 0;
+    cartas = [];
+    while(i < qtd){
+        cartas[i] = `<li onclick="virarCarta()"><img src="./imagens/front.png" alt="papagaio"></li>`;
+        i++;
+    }
+    distribuir.innerHTML = cartas;
 }
 
 
-
 function virarCarta(){
-
+    const virar = document.querySelector("li:nth-child(4) img");
+    virar.setAttribute("src", "./imagens/bobrossparrot.gif");
 }
