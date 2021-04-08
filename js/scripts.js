c5d0c20c78f8dd52f8ff319a4a5586df13fa5f96
Fernando Valani Cavalcante <valanife@gmail.com>
2021-04-08 00:09:05 -0300

    jogo funcionando

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,10 @@
 let qtd = 0;
+
+let jogadas = 0;
+let vitoria = 0;
+let iguais = [];
+
+
 jogar();
 function jogar(){
     /*while(qtd > 14 | qtd < 4){
@@ -7,32 +13,95 @@ function jogar(){
             qtd = 0;
         }
     }*/
-    qtd = 6;
-    //document.getElementsByClassName('container').style.width = "100px";
+    qtd = 14;
     distribuirCartas();
 }
 
 function distribuirCartas(){
     const distribuir = document.querySelector("ul");
+    const cartas = [];
     i = 0;
-    cartas = [];
-    while(i < qtd){
-        cartas[i] = `<li onclick="virarCarta(${i})"><img src="./imagens/front.png" alt="papagaio"></li>`;
+    while(i < qtd/2){
+        cartas.push(`<li class="${'carta' + i}" onclick="virarCarta(this)"><img src="./imagens/front.png" alt="papagaio"></li>`);
+        cartas.push(`<li class="${'carta' + i}" onclick="virarCarta(this)"><img src="./imagens/front.png" alt="papagaio"></li>`);
         i++;
     }
-    distribuir.innerHTML = cartas;
+
+    cartas.sort(comparador);
+    distribuir.innerHTML = cartas.join("");
 }
 
+function comparador(){
+    return Math.random() - 0.5;
+}
 
-let virada = 0;
 function virarCarta(n){
-    const valor = n + 1;
-    if(virada < 2){
-        const virar = document.querySelector("li:nth-child("+valor+") img");
-        virar.setAttribute("src", "./imagens/bobrossparrot.gif");
-        virada = virada + 1;
-        console.log(virada);
-    } else{
-        alert("duas cartas já selecionadas");
+    const virar = n.querySelector("img");
+
+    if(iguais.length < 2){
+
+        n.classList.add('selecionado');
+
+        if(n.classList.contains('carta0')){
+            virar.setAttribute("src", "./imagens/bobrossparrot.gif");
+            n.removeAttribute("onclick");
+            jogadas = jogadas + 1;
+        }
+        else if(n.classList.contains('carta1')){
+            virar.setAttribute("src", "./imagens/explodyparrot.gif");
+            n.removeAttribute("onclick");
+            jogadas = jogadas + 1;
+        }
+        else if(n.classList.contains('carta2')){
+            virar.setAttribute("src", "./imagens/fiestaparrot.gif");
+            n.removeAttribute("onclick");
+            jogadas = jogadas + 1;
+        }
+        else if(n.classList.contains('carta3')){
+            virar.setAttribute("src", "./imagens/metalparrot.gif");
+            n.removeAttribute("onclick");
+            jogadas = jogadas + 1;
+        }
+        else if(n.classList.contains('carta4')){
+            virar.setAttribute("src", "./imagens/tripletsparrot.gif");
+            n.removeAttribute("onclick");
+            jogadas = jogadas + 1;
+        }
+        else if(n.classList.contains('carta5')){
+            virar.setAttribute("src", "./imagens/revertitparrot.gif");
+            n.removeAttribute("onclick");
+            jogadas = jogadas + 1;
+        }
+        else if(n.classList.contains('carta6')){
+            virar.setAttribute("src", "./imagens/unicornparrot.gif");
+            n.removeAttribute("onclick");
+            jogadas = jogadas + 1;
+        }
+    }
+
+    iguais = document.querySelectorAll(".selecionado");
+
+    if(iguais.length === 2){
+        if(iguais[0].innerHTML === iguais[1].innerHTML){
+            iguais[0].classList.remove('selecionado');
+            iguais[1].classList.remove('selecionado');
+            vitoria = vitoria + 1;
+            iguais = [];
+        }else{
+            iguais[0].classList.remove('selecionado');
+            iguais[1].classList.remove('selecionado');
+            iguais[0].setAttribute("onclick", "virarCarta(this)");
+            iguais[1].setAttribute("onclick", "virarCarta(this)");
+            const virarVolta = iguais[0].querySelector("img");
+            virarVolta.setAttribute("src","./imagens/front.png");
+            const voltar = iguais[1].querySelector("img");
+            voltar.setAttribute("src","./imagens/front.png");
+            iguais = [];
+        }
+    }
+    console.log(jogadas);
+    if(vitoria === qtd/2){
+    alert("Você venceu o jogo em " + jogadas + " jogadas!!!");
     }
 }
+
